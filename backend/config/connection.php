12dafe4cae816d0e1a33a42738350fcb97ae6536
tmdkhac12 <quannguyen081204@gmail.com>
2025-04-19 10:48:31 +0700

    Merge vinh

--- backend/config/connection.php
+++ backend/config/connection.php
@@ -5,7 +5,7 @@ function getConnection()
     $username = "root";
     $password = "";
 
-    $connection = mysqli_connect($host, $username, $password, "db");
+    $connection = mysqli_connect($host, $username, $password, "web_nuochoa");
 
     if (!$connection) {
         echo "Connection failed: " . mysqli_connect_error();
